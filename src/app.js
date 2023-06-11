7fb8f1b61618358d2d91e61dd1cb11f63de0d5d9
Shannonmvr <127759428+Shannonmvr@users.noreply.github.com>
2023-06-11 19:56:12 +0200

    added default search city

--- src/app.js
+++ src/app.js
@@ -55,5 +55,7 @@ function handleSubmit(event) {
   search(cityInputElement.value);
 }
 
+search("Berlin");
+
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
