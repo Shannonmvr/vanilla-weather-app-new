7a6ced3c648c1d66ade1152f5bcbc31741921fb8
Shannonmvr <127759428+Shannonmvr@users.noreply.github.com>
2023-06-11 12:17:32 +0200

    Updated apiUrl

--- src/app.js
+++ src/app.js
@@ -28,6 +28,7 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
@@ -37,8 +38,8 @@ function displayTemperature(response) {
 }
 
 let apiKey = "80ab7od3ab75f07480betdd57ff51e6b";
-let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query=New York&key=80ab7od3ab75f07480betdd57ff51e6b&units=metric";
+let city = "Berlin";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
 console.log(apiUrl);
 
